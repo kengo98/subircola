c4d5ef44a0cc0824aee52e311f9a1ba461416bcd
KENGO NAKAMURA CHIBANA <kengonc98@gmail.com>
2020-03-10 20:02:31 -0400

    terminado.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace cola1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string ServiceBusConnectionString = "Endpoint=sb://computacionmovil.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+xyuzmOBvcYcw22+hIyF87FcoNWGr+hkVBJMdLbB9SY=";
+            string QueueName = "qprueba";
+            IQueueClient queueClient;
+
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            try
+            {
+                string messageBody = $"hola esto es un mensaje";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                await queueClient.SendAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            await queueClient.CloseAsync();
+
+        }
+    }
+
+}
+
+
